5b53941722d26580b6991cca8d90be1fd6cfbd4c
Maxim Lykov <frise.on@gmail.com>
2020-06-16 00:56:04 +0300

    menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,6 @@ const buttonMenu = document.querySelector('.main-menu__button');
 const menuList = document.querySelector('.main-menu_phone .main-menu__list');
 
 buttonMenu.addEventListener('click', (event) => {
+    buttonMenu.classList.toggle('main-menu__button_close');
     menuList.classList.toggle('main-menu__list_hidden');
 });
