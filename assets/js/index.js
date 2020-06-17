6b66d7fee419e022460ad4f7326a1ef72f2c84ed
Maxim Lykov <frise.on@gmail.com>
2020-06-17 16:20:12 +0300

    spotlight

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,19 @@
 const buttonMenu = document.querySelector('.main-menu__button');
 const menuList = document.querySelector('.main-menu_phone .main-menu__list');
+const spotlightContainer = document.querySelector('.spotlight-container');
+const spotlight = document.querySelector('.spotlight');
 
 buttonMenu.addEventListener('click', (event) => {
     buttonMenu.classList.toggle('main-menu__button_close');
     menuList.classList.toggle('main-menu__list_hidden');
+});
+
+spotlightContainer.addEventListener('mousemove', (e) => {
+    const spotlightParams = spotlight.getBoundingClientRect();
+    const w = spotlightParams.width;
+    const h = spotlightParams.height;
+    const t = e.pageY - spotlightParams.top;
+    const l = e.pageX - spotlightParams.left;
+
+    spotlight.style.backgroundImage = `radial-gradient(circle at ${l / w * 100}% ${t / h * 100}%, transparent 80px, rgba(0, 0, 0, 0.8) 120px)`;
 });
